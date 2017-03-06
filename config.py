2315e3bfa62e0cb460a19cab66630c44ce2debe8
Alexander <yoyo.aero@yandex.ru>
2017-03-06 22:05:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = 'insert token'
+token = 'insert token' #Вставить HTTP-Token бота вместо insert token
 password = 'password'  # Вставить пароль для входа вместо password
 admin = 'admin'  # Вставить пароль для входа в админку вместо admin
