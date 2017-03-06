8896d15334408b1aaf47d429c4989307e3aaaee4
Alexander <yoyo.aero@yandex.ru>
2017-03-06 22:05:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = '340329443:AAEVzV0xig6UZuCEHH7GnmKIs0-IrS88RZo'
+token = 'insert token'
 password = 'password'  # Вставить пароль для входа вместо password
 admin = 'admin'  # Вставить пароль для входа в админку вместо admin
