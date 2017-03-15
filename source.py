c59c785e9cf59f640e0640b64435cf2fdd3ca551
Alexander <yoyo.aero@yandex.ru>
2017-03-15 22:42:04 +0300

    v 1.1.2
    Мелкие исправления.

--- source.py
+++ source.py
@@ -640,7 +640,7 @@ checker()
 @bot.message_handler(func=lambda message: (message.content_type == 'text'))
 def start(message):
     if ifregadmin(message.chat.id):
-            adminlog(message)
+        adminlog(message)
     elif ifreg(message.chat.id):
         userlog(message)
     else:
