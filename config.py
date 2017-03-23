948755f1074549fdcdd2664b68b939294d985fe9
Alexander <yoyo.aero@yandex.ru>
2017-03-23 17:53:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '340329443:AAEVzV0xig6UZuCEHH7GnmKIs0-IrS88RZo' # Вставить Token бота вместо insert_token
+token = 'insert_token' # Вставить Token бота вместо insert_token
 password = 'password'  # Вставить пароль для входа вместо password
 admin = 'admin'  # Вставить пароль для входа в админку вместо admin
 parents = 'parents' #Вставить пароль для входа в аккаунт родителя вместо parents
